3c317c7b21b206253d1c0b5357f3ff5945e18362
moiseschungazapata-glitch <moiseschungazapata@gmail.com>
2026-09-23 21:56:54 -0500

    Delete backend directory

--- backend/app/maintenance.py
+++
@@ -1,5 +0,0 @@
-"""Run from a trusted server scheduler using its environment, not a public cron URL."""
-from app.database.connection import db
-
-if __name__ == '__main__':
-    print(db.rpc('aplicar_retencion', {'p_actor': None}))
